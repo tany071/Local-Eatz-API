71aaad791f58dd7bfbb6665cad6baa13d264c6c7
Tanya Sinha <tanyasinha7u@gmail.com>
2024-02-21 12:24:09 +0530

    added post review route

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+
+var data = require('./data.json');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3002
+
+// console.log(data.restaurants[0].restaurantReviews)
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  // structure for req { id , user , rating(input) ,review(input) }
+
+  data.restaurants[req.body.id].restaurantReviews.push({ "username": req.body.user , "reviewDate": Date.now , "userRating": req.body.rating ,"userReview": req.body.review })
+  console.log("Data Added Successfully")
+
+
+  res.send('Hello World')
+})
+
+
+app.get('/review', (req,res)=>{
+  res.send("Hello From Review")
 })
 
 
